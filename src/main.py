b441f3334ac937583f49995a0d432585a66c66d8
Marton Gergely <61601074+D3w4r@users.noreply.github.com>
2021-04-14 15:42:03 +0200

    Hidden API keys for spotipy, added logging, added tts.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-from client import Client
+import time
+
+from myradio.src import weather
+from myradio.src.client import Client
+
+
+def print_artist_info(artist):
+    print(artist['name'])
+    print(str(artist['followers']['total']) + " followers")
+    print(artist['genres'][0])
+    print()
+
+
+weatherApp = weather.Weather('Budapest')
+weatherApp.synthesise(r"D:\Data\ProjectLaboratory\myradio\src\weather.wav")
 
 username = 'dewarhun'
-
 client = Client(username)
 
 # Devices
@@ -9,7 +22,7 @@ devices = client.getActiveDevices()
 # Set primary device
 client.setPrimaryDeviceId(devices, 0)
 # Current track information
-current_track = client.getCurrentTrack()
+current_track = client.getCurrentTrack('v')
 artist = client.getArtistOfTrack(current_track)
 print(artist)
 # User info
@@ -30,17 +43,13 @@ while True:
     # Search for artist
     if choice == '0':
 
-        print()
         searchQuery = input("Ok, what's their name?:")
-        print()
         # Get search results
         searchResults = client.search(searchQuery, 1, 0, 'artist')
         # Print artist details
         artist = searchResults['artists']['items'][0]
-        print(artist['name'])
-        print(str(artist['followers']['total']) + " followers")
-        print(artist['genres'][0])
-        print()
+
+        print_artist_info(artist)
 
         artist_id = artist['id']
 
@@ -49,10 +58,28 @@ while True:
         selected = []
         while True:
             selected.append(client.selectSong(all_tracks))
-            if selected == None:
+            if selected is None:
                 break
             else:
+                # Start playback
                 client.start_playback(None, selected)
+                # Wait 10 seconds
+                time.sleep(10)
+                # Get info about the current track
+                current_track = client.getCurrentTrack()
+                progress_ms = current_track['progress_ms']
+                name = current_track['item']['name']
+                # Stop playback
+                print(f"Stopping last track: {name}")
+                # Play sample audio
+                client.pause_playback()
+
+                client.simulate_speech()
+
+                print(f"Continuing last track: {name}")
+
+                client.start_playback(context_uri=None, uris=selected, progress_ms=progress_ms)
+
                 selected.pop()
 
     if choice == '1':
--- src/weather.py
+++ src/weather.py
@@ -1,13 +1,16 @@
-import requests
 import json
-from geopy.geocoders import Nominatim
-from azure.cognitiveservices.speech import AudioDataStream, SpeechConfig, SpeechSynthesizer, SpeechSynthesisOutputFormat
+import logging
+
+import requests
+from azure.cognitiveservices.speech import SpeechConfig, SpeechSynthesizer
 from azure.cognitiveservices.speech.audio import AudioOutputConfig
+from geopy.geocoders import Nominatim
 
 
 class Weather:
     """This class manages weather info through the OpenWeatherMap API"""
 
+    logger = logging.getLogger('Logger')
     api_key = 'fccb3b5153cacfb74847f4c9fd8ec730'
     speech_config = SpeechConfig(subscription='792a41e8595a4992a02a06c58db64d0f', region='westeurope')
 
@@ -18,10 +21,12 @@ class Weather:
         return self.city
 
     def getLocation(self):
+        self.logger.info(f'Getting coordinates for {self.city}')
         geolocator = Nominatim(user_agent='weatherapp')
-        return geolocator.geocode('Budapest').latitude, geolocator.geocode(self.city).longitude
+        return geolocator.geocode(self.city).latitude, geolocator.geocode(self.city).longitude
 
     def getWeatherInfo(self):
+        self.logger.info('Getting current weather info...')
         location = self.getLocation()
         url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (
             location[0], location[1], self.api_key)
@@ -31,14 +36,15 @@ class Weather:
 
     def getCurrentWeatherText(self):
         data = self.getWeatherInfo()
-
+        self.logger.info('Creating sentence for TTS interface...')
         text = "It's currently " + str(data["current"]["temp"]) + " degrees, but it feels like " + str(
             data["current"]["feels_like"]) + " degrees outside. Expect " + str(
             data["current"]["weather"][0]["main"]) + " weather. Wind speed is " + str(
             data["current"]["wind_speed"]) + " kilometers an hour."
         return text
 
-    def synthetise(self, path):
+    def synthesise(self, path):
+        self.logger.info('Synthesising speech')
         audio_config = AudioOutputConfig(filename=path)
         synthesizer = SpeechSynthesizer(speech_config=self.speech_config, audio_config=audio_config)
         synthesizer.speak_text_async(self.getCurrentWeatherText())
