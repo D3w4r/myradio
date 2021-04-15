45cf3f89b752af25d656dced3bbde6c1eff520d0
Marton Gergely <61601074+D3w4r@users.noreply.github.com>
2021-04-15 19:32:57 +0200

    Gmail API finished

--- src/mail.py
+++ src/mail.py
@@ -1,33 +1,31 @@
-
+import base64
 import os
 import pickle
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+
 from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
 
 
 class Gmail:
-    # user = 'martongergely11@gmail.com'
-    # passw = '148785NKFc2wns'
-
     SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 
     def __init__(self):
         self.credentials = None
 
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
+        if os.path.exists('auth/token.pickle'):
+            with open('auth/token.pickle', 'rb') as token:
                 self.credentials = pickle.load(token)
 
         if not self.credentials or not self.credentials.valid:
             if self.credentials and self.credentials.expired and self.credentials.refresh_token:
                 self.credentials.refresh(Request())
             else:
-                self.flow = InstalledAppFlow.from_client_secrets_file('credentials.json', self.SCOPES)
+                self.flow = InstalledAppFlow.from_client_secrets_file('auth/credentials.json', self.SCOPES)
                 self.credentials = self.flow.run_local_server(port=8080)
 
             # Save acces token
-            with open('token.pickle', 'wb') as token:
+            with open('auth/token.pickle', 'wb') as token:
                 pickle.dump(self.credentials, token)
 
     def get_emails(self):
@@ -35,10 +33,45 @@ class Gmail:
         service = build('gmail', 'v1', credentials=self.credentials)
 
         # Request a list of all messages
+        result = service.users().messages().list(maxResults=5, userId='me').execute()
 
+        # Messages is a list of dicts, where each dict contains a message ID
+        messages = result.get('messages')
 
+        # Iterate over it
+        for msg in messages:
+            txt = service.users().messages().get(userId='me', id=msg['id']).execute()
 
+            try:
+                # Get value of 'payload' from dictionary 'txt'
+                payload = txt['payload']
+                headers = payload['headers']
+
+                # Look for Subject and Sender Email in the headers
+                for d in headers:
+                    if d['name'] == 'Subject':
+                        subject = d['value']
+                    if d['name'] == 'From':
+                        sender = str(d['value'])
+                        sender = sender.replace('\"', "").split(" ")[0]
+
+                # The Body of the message is in Encrypted format. So, we have to decode it.
+                # Get the data and decode it with base 64 decoder.
+                # pprint(payload.get('parts'))
+                body = payload.get('body')
+                data = str(body.get('data'))
+                data = data.replace("-", "+").replace("_", "/")
+                decoded_data = base64.b64decode(data)
+
+                # Printing the subject, sender's email and message
+                print("Subject: ", subject)
+                print("From: ", sender)
+                print('\n')
+
+            except:
+                raise Exception("Error whilst getting messages!")
 
 
 if __name__ == "__main__":
-    pass
+    gmail = Gmail()
+    gmail.get_emails()
