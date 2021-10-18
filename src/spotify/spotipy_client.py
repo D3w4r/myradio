0a41f4a086658330b1e4e8ed6790909f0f1cc46f
Marton Gergely <61601074+D3w4r@users.noreply.github.com>
2021-10-18 14:50:47 +0200

    removed unused code

--- src/spotify/spotipy_client.py
+++ src/spotify/spotipy_client.py
@@ -18,7 +18,7 @@ class Client:
                  ):
         self.username = username
         self.spotifyObject = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-        self.stopped_track = None
+        self.current_track = self.get_current_track()
 
     def active_devices(self):
         """
@@ -42,76 +42,6 @@ class Client:
 
         self.device = devices['devices'][idx]['id']
 
-    def get_primary_device(self):
-        """
-        Get primary device
-
-        :return: The device used to modify playback state
-        """
-
-        return self.device
-
-    def current_track(self, verbose=None):
-        """
-        Get current track information
-
-        :param verbose: if set, it prints the info on the console.
-        :return: the currently playing track
-        """
-
-        track = self.spotifyObject.current_user_playing_track()
-        if track is None:
-            print('No track is playing')
-        elif verbose == 'v':
-            print(f"Currently playing track: -- {track['item']['name']} by {track['item']['artists'][0]['name']}")
-        return track
-
-    def get_artist(self, track):
-        """
-        Get the artist of the given track.
-
-        :param track: the track, of which artist you want to get.
-        :return: the artist
-        """
-        return track['item']['artists'][0]['name']
-
-    def get_tracks_by_artist(self, artist_id):
-        """
-        Get tracks of the given artist.
-
-        :param artist_id: the artist, whom tracks you want to get.
-        :return: every track of that artist
-        """
-        trackURIs = []
-        idx = 0
-
-        albums = self.spotifyObject.artist_albums(artist_id)
-        albums = albums['items']
-
-        for item in albums:
-            print(f"Album: {item['name']}")
-            albumId = item['id']
-
-            trackResults = self.spotifyObject.album_tracks(albumId)
-            trackResults = trackResults['items']
-
-            for item in trackResults:
-                print(str(idx) + ": " + item['name'])
-                trackURIs.append(item['uri'])
-                idx += 1
-            print()
-
-        return trackURIs
-
-    def user(self):
-        """
-        Get current user info
-
-        :return: the currently active user.
-        """
-        user = self.spotifyObject.current_user()
-        return user
-
     def search(self, query, limit, offset, searchType='track'):
         """
         Search query using Spotipy API
@@ -130,31 +60,23 @@ class Client:
                                           position_ms=progress_ms)
 
     def restart_playback(self):
-        uri = [self.stopped_track['item']['uri']]
-        progress_ms = self.stopped_track['progress_ms']
+        uri = [self.current_track['item']['uri']]
+        progress_ms = self.current_track['progress_ms']
         self.spotifyObject.start_playback(device_id=self.device, uris=uri, offset=None,
                                           position_ms=progress_ms)
 
-    def select_song(self, from_tracks):
-        """
-        Selects track uri from the given list of tracks
-        :param from_tracks:
-        :return: uri of the selected track
-        """
-        selection = input("Enter a song number: ")
-        if selection == 'x':
-            return None
-        return from_tracks[int(selection)]
+    def get_current_track(self):
+        return self.spotifyObject.current_user_playing_track()
 
     def pause_playback(self):
         """
         Pauses playback and returns stopped track
         """
-        stopped_track = self.current_track()
+        stopped_track = self.current_track
         name = stopped_track['item']['name']
         self.spotifyObject.pause_playback(self.device)
         logging.info(f"Stopping last track: {name}")
-        self.stopped_track = stopped_track
+        self.current_track = stopped_track
 
 
 if __name__ == "__main__":
