6a3d732e62f6939d7fb6b5c431c5307e7dd3c2e9
Marton Gergely <61601074+D3w4r@users.noreply.github.com>
2021-11-22 21:14:23 +0100

    keyboardinterrupt

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import json
-import logging
+import sys
 
 import google.cloud.texttospeech as tts
+import keyboard
 import multitimer
 
 from src.data.enums import Constants
@@ -11,24 +12,26 @@ from src.text_to_speech import google_speech
 from src.text_to_speech.speech import Speech
 from src.text_to_speech.text_generator import TextGenerator
 
-logging.basicConfig(level=logging.INFO)
+
+def get_interval():
+    with open(Constants.CONFIG.value) as config:
+        _config = json.load(config)
+        return _config['news']['interval_sec']
 
 
-def initialize():
-    interval = None
-    with open(Constants.CONFIG.value) as basic:
-        basic_config = json.load(basic)
-        if basic_config['speech']['resource'] == 'azure':
-            language = basic_config['azure']['language']
-            voice = language + '-' + basic_config['azure']['voice']
+def initialize_speech():
+    with open(Constants.CONFIG.value) as config:
+        _config = json.load(config)
+        if _config['speech']['resource'] == 'azure':
+            language = _config['azure']['language']
+            voice = language + '-' + _config['azure']['voice']
             speech = azure_speech.AzureSpeech(language=language, voice=voice)
-        elif basic_config['speech']['resource'] == 'google':
-            language = basic_config['google']['language']
-            name = language + '-' + basic_config['google']['voice']
+        elif _config['speech']['resource'] == 'google':
+            language = _config['google']['language']
+            name = language + '-' + _config['google']['voice']
             speech = google_speech.GoogleSpeech(
                 voice_params=tts.VoiceSelectionParams(name=name, language_code=language))
-        interval = basic_config['news']['interval_sec']
-    return speech, interval
+    return speech
 
 
 def demonstrate(spotify: Client, generator: TextGenerator, speech: Speech):
@@ -43,7 +46,8 @@ def main():
     client = Client('dewarhun')
 
     generator = TextGenerator()
-    speech, interval = initialize()
+    speech = initialize_speech()
+    interval = get_interval()
 
     timer = multitimer.MultiTimer(interval=interval, function=demonstrate, args=[client, generator, speech],
                                   runonstart=False)
@@ -51,4 +55,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
