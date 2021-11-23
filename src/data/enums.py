d6b2fe34f88f358ef09a4704a56ad8b04f9a4543
Marton Gergely <61601074+D3w4r@users.noreply.github.com>
2021-11-23 12:55:26 +0100

    dev patch

--- src/data/enums.py
+++ src/data/enums.py
@@ -1,14 +1,15 @@
+import os
 from enum import Enum
 
 
 class Constants(Enum):
-    CACHE_PATH = 'src/cache'
+    CACHE_PATH = os.getcwd() + '/cache'
     RSS_REPOSITORY = CACHE_PATH + '/rss_repository'
     CITY_LIST = CACHE_PATH + '/current.city.list.json'
     CITY_LIST_GZ = CACHE_PATH + '/current.city.list.json.gz'
-    CONFIG = 'src/config/basic_config.json'
+    CONFIG = os.getcwd() + '/config/basic_config.json'
     MAIL_REPOSITORY = CACHE_PATH + '/repository.json'
-    SAVED_WEIGHTS = 'src/dnn/saved-weigths.h5'
+    SAVED_WEIGHTS = os.getcwd() + '/dnn/saved-weigths.h5'
 
 
 class Time(Enum):
--- src/dnn/model.py
+++ src/dnn/model.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import numpy as np
 import tensorflow as tf
@@ -12,7 +13,7 @@ class CustomDnn:
     def __init__(self):
         self.tokenizer = AutoTokenizer.from_pretrained("roberta-base")
         self.trained_model = TFAutoModelForSequenceClassification.from_pretrained("roberta-base", num_labels=28)
-        self.trained_model.load_weights('saved-weights.h5')
+        self.trained_model.load_weights(os.getcwd() + 'dnn/saved-weights.h5')
         self.logger = logging.getLogger(__name__)
 
     def predict(self, texts: list):
