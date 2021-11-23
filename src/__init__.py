a99ee632d4a72e021d0376db4f546ee031cb5d0c
Marton Gergely <61601074+D3w4r@users.noreply.github.com>
2021-11-23 10:29:26 +0100

    change cwd

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,6 @@
+import os
 import logging.config
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 logging.basicConfig(format='%(asctime)s : %(lineno)d - %(message)s', level=logging.INFO, filename='src/logs.log', filemode='w')
 logger = logging.getLogger(__name__)
